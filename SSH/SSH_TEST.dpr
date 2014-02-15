84a56ae750817f9d122dbcddc625e350adfb1f6f
DelphiXE <alex0380@mail.ru>
2014-02-16 00:35:21 +0600

    SSH
    Пробная работа с протоколом SSH

---
+++ SSH/SSH_TEST.dpr
@@ -0,0 +1,34 @@
+program SSH_TEST;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  SSHclient;
+
+var
+   lSSh : TSSHClient;
+
+begin
+  try
+   lSSh := TSSHClient.Create('95.78.251.16', '222', 'mate', 'Uafax8Ie');
+    { TODO -oUser -cConsole Main : Insert code here }
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+  if lSSh.login then
+   begin
+     writeln('Подключился!');
+     writeln(lSSh.ReceiveData);
+     lSSh.SendCommand('df -h');
+     writeln(lSSh.ReceiveData);
+     lSSh.LogOut;
+     writeln('Отключился.');
+   end
+   else
+    writeln('Не могу подключиться.');
+   lSSh.Free;
+end.
